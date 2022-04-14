aa68b71744c6fa795a9bff9e3588820863bf4bbc
sean-pwd <sean@powerfulwebdesign.com.au>
2022-04-13 22:49:53 -0500

    fix: update

--- assets/js/assets.js
+++ assets/js/assets.js
@@ -1,4 +1,4 @@
 // import core-js so babel can provide necessarty polyfills
 // import 'core-js/stable';
 // apply year to footer
-document.getElementById('year').innerHTML = new Date().getFullYear();
+// document.getElementById('year').innerHTML = new Date().getFullYear();
--- assets/js/vendor.js
+++ assets/js/vendor.js
@@ -1,1 +1,3 @@
 import image from 'js/lazysizes';
+
+import {Dropdown} from 'bootstrap';
