a5c1f161af5b68c2f379a2e6dfbb806eb0cef678
Sean Emerson <emerson.sean.dev@gmail.com>
2022-06-29 13:46:26 -0700

    add no-js

--- assets/js/assets.js
+++ assets/js/assets.js
@@ -1,3 +1,5 @@
+document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/, 'js');
+
 // apply year to footer
 const year = document.getElementById('year');
 if (year) {
--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,47 +1,50 @@
 const autoprefixer = require('autoprefixer');
-// const purgecss = require('@fullhuman/postcss-purgecss');
-// const whitelister = require('purgecss-whitelister');
+const purgecss = require('@fullhuman/postcss-purgecss');
+const whitelister = require('purgecss-whitelister');
+
+function production() {
+  autoprefixer();
+  // purgecss({
+  //   content: [
+  //     './layouts/**/*.html',
+  //     './content/**/*.md',
+  //     //'./assets/js/**/*.js',
+  //   ],
+  //   //defaultExtractor: 
+  //   //   // keyframes: true
+  //   //   //variables: false,
+  //   safelist: {
+  //     standard: [
+  //       /^.hugo-img/,
+  //       /^.js/,
+  //       ':root',
+  //       'table',
+  //       'thead',
+  //       'tbody',
+  //       'tr',
+  //       'th',
+  //       'td',
+  //       'h3',
+  //       ...whitelister([
+  //         // './assets/scss/components/_buttons.scss',
+  //         //'./assets/scss/components/_code.scss',
+
+  //         // './assets/scss/components/_syntax.scss',
+  //       ]),
+  //     ],
+  //     deep: [],
+  //     greedy: [],
+  //     keyframes: [],
+  //     variables: [],
+  //   },
+  // })
+}
 
 module.exports = {
   plugins: [
     // the following runs when env is NOT development
-    !process.env.HUGO_ENVIRONMENT === 'development'
-    ? autoprefixer()
-    : null,
-    // purgecss({
-    //   content: [
-    //     './layouts/**/*.html',
-    //     './content/**/*.md',
-    //     './assets/js/**/*.js',
-    //   ],
-    //   //defaultExtractor: 
-    // //   // keyframes: true
-    // //   //variables: false,
-    // //   safelist: {
-    // //     standard: [
-    // //       /^.hugo-img/, 
-    // //       /^.js/,
-    // //       ':root',
-    // //       'table',
-    // //       'thead',
-    // //       'tbody',
-    // //       'tr',
-    // //       'th',
-    // //       'td',
-    // //       'h3',
-    // //       ...whitelister([
-    // //         // './assets/scss/components/_buttons.scss',
-    // //         //'./assets/scss/components/_code.scss',
-        
-    // //         // './assets/scss/components/_syntax.scss',
-    // //       ]),
-    // //     ],
-    // //     deep: [],
-    // //     greedy: [],
-    // //     keyframes: [],
-    // //     variables: [],
-    // //   },
-    //  }
-    //  ),
+    // !process.env.HUGO_ENVIRONMENT === 'development'
+    //   ? production() : null,
   ],
 }
+// disabled plugins as node modules needs to be reinstalled  
